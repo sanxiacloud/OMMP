0012ff686657351688a2452d18e40f9fcf7b9518
andylinky <andy_linky@qq.com>
2019-06-03 08:24:02 +0800

    修改 为  ommp.dto.dll & ommp.dao.dll

---
+++ dll/ommp/dao/ApplicationSolutionDAO.vb
@@ -0,0 +1,153 @@
+﻿Imports ommp.dto
+Imports Foxtable
+
+Namespace dao
+    Public Class ApplicationSolutionDAO
+
+        Private Const TABLE_FunctionalCI As String = "FunctionalCI"
+        Private Const TABLE_ApplicationSolution As String = "ApplicationSolution"
+
+        Private Function SetProperties(ByVal drFCI As DataRow, ByVal drAS As DataRow) As ApplicationSolution
+            Dim item As New ApplicationSolution()
+
+            item.Identify = drFCI("_Identify")
+            item.name = drFCI("name")
+            item.description = drFCI("description")
+            item.code_risk_rating = drFCI("code_risk_rating")
+            item.move2production = drFCI("move2production")
+            item.finalclass = drFCI("finalclass")
+            item.obsolescence_date = drFCI("obsolescence_date")
+            item.IsDeleted = drFCI("_IsDeleted")
+
+            item.id = drAS("id")
+            item.code_application_status = drAS("code_application_status")
+            item.redundancy = drAS("redundancy")
+            item.code_sla = drAS("code_sla")
+            item.fault_effects = drAS("fault_effects")
+            item.code_sla = drAS("code_sla")
+            item.attention = drAS("attention")
+            item.IsDeleted = drAS("_IsDeleted")
+
+            Return item
+        End Function
+
+        'Relations 表示关联集合，可以获得指定名称的关联
+        'Add(RelationName, ParentCol, ChildCol,Visible, RelationPath) 
+        'RelationName:    关联名称
+        'ParentCol:       父表关联列
+        'ChildCol:        子表关联列
+        'Visible:         可选参数,是否显示关联表  
+        'RelationPath:    RelationPathEnum型枚举，用于设置关联表生成模式，有三个可选值，分别是:One(单向生成),Both(双向生成),None(不生成)
+        Public Function FindList(ByVal filter As String, ByVal sort As String) As IList(Of ApplicationSolution)
+            Dim lists As IList(Of ApplicationSolution) = New Generic.List(Of ApplicationSolution)()
+            Relations.Add("rASofFCI", DataTables(TABLE_FunctionalCI).DataCols("_Identify"), DataTables(TABLE_ApplicationSolution).DataCols("id"), True, RelationPathEnum.One)
+            Dim re As Relation = Relations("rASofFCI")
+
+            Try
+                Dim drs As List(Of DataRow)
+                If sort IsNot Nothing Then
+                    drs = DataTables(TABLE_NAME).Select(filter, sort)
+                Else
+                    drs = DataTables(TABLE_NAME).Select(filter)
+                End If
+                For Each dr As DataRow In drs
+                    lists.Add(SetProperties(dr))
+                Next
+            Catch ex As Exception
+                Output.Show(TABLE_NAME & "->FindList:" & ex.Message)
+            End Try
+
+            Return lists
+        End Function
+
+        Public Function FindObject(ByVal id As Integer) As ApplicationSolution
+            Dim item As New ApplicationSolution()
+            Try
+                Dim drAS As DataRow = DataTables(TABLE_ApplicationSolution).Find("id = " & id)
+                Dim drFCI As DataRow = DataTables(TABLE_FunctionalCI).Find("_Identify = " & id)
+
+                If drAS IsNot Nothing And drFCI IsNot Nothing Then
+                    item = SetProperties(drFCI, drAS)
+                End If
+
+            Catch ex As Exception
+                Output.Show(TABLE_ApplicationSolution & "->FindObject:" & ex.Message)
+            End Try
+
+            Return item
+        End Function
+
+        Public Function Delete(ByVal id As Integer) As Boolean
+            Dim result As Boolean = False
+            Try
+                Dim drAS As DataRow = DataTables(TABLE_ApplicationSolution).Find("id = " & id)
+                drAS("_IsDeleted") = True
+                drAS.Save()
+
+                Dim drFCI As DataRow = DataTables(TABLE_FunctionalCI).Find("_Identify = " & id)
+                drFCI("_IsDeleted") = True
+                drFCI.Save()
+
+                result = True
+            Catch ex As Exception
+                Output.Show(TABLE_ApplicationSolution & "->Delete:" & ex.Message)
+                Output.Show(TABLE_FunctionalCI & "->Delete:" & ex.Message)
+            End Try
+
+            Output.Show("Delete is invoked successful!")
+
+            Return result
+        End Function
+
+        Public Function Insert(ByVal obj As ApplicationSolution) As Boolean
+            Dim result As Boolean = False
+
+            Try
+                Dim drFCI As DataRow = DataTables(TABLE_FunctionalCI).AddNew()
+
+                drFCI("name") = obj.name
+                drFCI("description") = obj.description
+                drFCI("code_risk_rating") = obj.code_risk_rating
+                drFCI("move2production") = obj.move2production
+                drFCI("finalclass") = TABLE_ApplicationSolution
+                drFCI("obsolescence_date") = obj.obsolescence_date
+                If obj.IsDeleted = True Or obj.IsDeleted = False Then
+                    drFCI("_IsDeleted") = obj.IsDeleted
+                Else
+                    drFCI("_IsDeleted") = False
+                End If
+
+                drFCI.Save()
+
+
+                Dim drAS As DataRow = DataTables(TABLE_ApplicationSolution).AddNew()
+
+                drAS("id") = drFCI("_Identify")
+                drAS("code_application_status") = obj.code_application_status
+                drAS("redundancy") = obj.redundancy
+                drAS("code_sla") = obj.code_sla
+                drAS("fault_effects") = obj.fault_effects
+                drAS("attention") = obj.attention
+                If obj.IsDeleted = True Or obj.IsDeleted = False Then
+                    drAS("_IsDeleted") = obj.IsDeleted
+                Else
+                    drAS("_IsDeleted") = False
+                End If
+
+                drAS.Save()
+
+                result = True
+            Catch ex As Exception
+                Output.Show(TABLE_ApplicationSolution & "->Insert:" & ex.Message)
+            End Try
+
+            Output.Show("Insert is invoked successful!")
+
+            Return result
+        End Function
+
+
+    End Class
+
+End Namespace
+
---
+++ dll/ommp/dao/OrganizationDAO.vb
@@ -0,0 +1,161 @@
+﻿Imports ommp.dto
+Imports Foxtable
+
+Namespace dao
+    Public Class OrganizationDAO
+
+        Private Const TABLE_NAME As String = "Organization"
+
+        Public Sub New()
+            ' 构造函数，默认为空
+        End Sub
+
+        Private Function SetProperties(ByVal dr As DataRow) As Organization
+            Dim item As New Organization()
+
+            item.Identify = dr("_Identify")
+            item.parent_identify = dr("parent_identify")
+            item.name = dr("name")
+            item.code = dr("code")
+            item.parent_code = dr("_code")
+            item.status = dr("status")
+            item.code_org_type = dr("code_org_type")
+            item.short_name = dr("short_name")
+            item.description = dr("description")
+            item.IsDeleted = dr("_IsDeleted")
+            item._sort = dr("_sort")
+
+            Return item
+        End Function
+
+        Public Function Insert(ByVal obj As Organization) As Boolean
+            Dim result As Boolean = False
+
+            Try
+                Dim dr As DataRow = DataTables(TABLE_NAME).AddNew()
+
+                dr("parent_identify") = obj.parent_identify
+                dr("name") = obj.name
+                dr("code") = obj.code
+                dr("_code") = obj.parent_code
+                dr("status") = obj.status
+                dr("code_org_type") = obj.code_org_type
+                dr("short_name") = obj.short_name
+                dr("description") = obj.description
+                If obj.IsDeleted = True Or obj.IsDeleted = False Then
+                    dr("_IsDeleted") = obj.IsDeleted
+                Else
+                    dr("_IsDeleted") = False
+                End If
+                If obj._sort > 0 Then
+                    dr("_sort") = obj._sort
+                End If
+
+                dr.Save()
+                result = True
+            Catch ex As Exception
+                Output.Show(TABLE_NAME & "->Insert:" & ex.Message)
+            End Try
+
+            Output.Show("Insert is invoked successful!")
+
+            Return result
+        End Function
+
+        Public Function FindList(ByVal filter As String, ByVal sort As String) As IList(Of Organization)
+            Dim lists As IList(Of Organization) = New Generic.List(Of Organization)()
+            Try
+                Dim drs As List(Of DataRow)
+                If sort IsNot Nothing Then
+                    drs = DataTables(TABLE_NAME).Select(filter, sort)
+                Else
+                    drs = DataTables(TABLE_NAME).Select(filter)
+                End If
+                For Each dr As DataRow In drs
+                    lists.Add(SetProperties(dr))
+                Next
+            Catch ex As Exception
+                Output.Show(TABLE_NAME & "->FindList:" & ex.Message)
+            End Try
+
+            Return lists
+        End Function
+
+        Public Function FindObject(ByVal id As Integer) As Organization
+            Dim item As New Organization()
+            Try
+                Dim dr As DataRow = DataTables(TABLE_NAME).Find("_Identify = " & id)
+                If dr IsNot Nothing Then
+                    item = SetProperties(dr)
+                End If
+            Catch ex As Exception
+                Output.Show(TABLE_NAME & "->FindObject:" & ex.Message)
+            End Try
+
+            Return item
+        End Function
+
+        Public Function Delete(ByVal id As Integer) As Boolean
+            Dim result As Boolean = False
+            Try
+                Dim dr As DataRow = DataTables(TABLE_NAME).Find("_Identify = " & id)
+                dr("_IsDeleted") = True
+                dr.Save()
+            Catch ex As Exception
+                Output.Show(TABLE_NAME & "->Delete:" & ex.Message)
+            End Try
+
+            Output.Show("Delete is invoked successful!")
+
+            Return result
+        End Function
+
+        Public Function Update(ByVal obj As Organization) As Boolean
+            Dim result As Boolean = False
+
+            Try
+                Dim dr As DataRow = DataTables(TABLE_NAME).Find("_Identify = " & obj.Identify)
+                If obj.parent_identify > 0 Then
+                    dr("parent_identify") = obj.parent_identify
+                End If
+                If obj.name IsNot Nothing Then
+                    dr("name") = obj.name
+                End If
+                If obj.code IsNot Nothing Then
+                    dr("code") = obj.code
+                End If
+                If obj.parent_code IsNot Nothing Then
+                    dr("_code") = obj.parent_code
+                End If
+                If obj.status = False Or obj.status = True Then
+                    dr("status") = obj.status
+                End If
+                If obj.code_org_type > 0 Then
+                    dr("code_org_type") = obj.code_org_type
+                End If
+                If obj.short_name IsNot Nothing Then
+                    dr("short_name") = obj.short_name
+                End If
+                If obj.description IsNot Nothing Then
+                    dr("description") = obj.description
+                End If
+                If obj.IsDeleted = False Or obj.IsDeleted = True Then
+                    dr("_IsDeleted") = obj.IsDeleted
+                End If
+                If obj._sort > 0 Then
+                    dr("_sort") = obj._sort
+                End If
+                dr.Save()
+                result = True
+            Catch ex As Exception
+                Output.Show(TABLE_NAME & "->Update:" & ex.Message)
+            End Try
+
+            Output.Show("Update is invoked successful!")
+
+            Return result
+        End Function
+
+    End Class
+
+End Namespace
