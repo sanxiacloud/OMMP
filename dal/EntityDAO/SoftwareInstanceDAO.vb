96591ebcd37ce40abc15fee6d2eb9cdf2a3d53f0
andylin <andy_linky@qq.com>
2019-06-17 17:30:25 +0800

    TestUpdate() ApplicationSolutionQT -> ApplicationSolution

---
+++ dal/EntityDAO/SoftwareInstanceDAO.vb
@@ -0,0 +1,3 @@
+﻿Public Class SoftwareInstanceDAO
+
+End Class
--- dal/LinkDAO/LnkApplicationSolutionToFunctionalCIDAO.vb
+++ dal/LinkDAO/LnkApplicationSolutionToFunctionalCIDAO.vb
@@ -6,22 +6,16 @@ Namespace dal.dao
         Inherits BaseDAO
         Implements ILinkDAO
 
-
         Public Sub New()
             ' 构造函数，默认为空
         End Sub
 
-        Private ReadOnly Property _TABLE_NAME() As String
-            Get
-                Return LnkApplicationSolutionToFunctionalCI.TABLE_NAME
-            End Get
-        End Property
-
         Public Function Link(ByVal fromId As Integer, ByVal toId As Integer) As Boolean Implements ILinkDAO.Link
+            Dim obj As New LnkApplicationSolutionToFunctionalCI()
             Dim result As Boolean = False
 
             Try
-                Dim dr As DataRow = DataTables(_TABLE_NAME).AddNew()
+                Dim dr As DataRow = DataTables(obj.GetType().Name).AddNew()
 
                 dr(LnkApplicationSolutionToFunctionalCI.C_APPLICATIONSOLUTION_IDENTIFY) = fromId
                 dr(LnkApplicationSolutionToFunctionalCI.C_FUNCTIONALCI_IDENTIFY) = toId
@@ -30,13 +24,16 @@ Namespace dal.dao
                 dr.Save()
                 result = True
             Catch ex As Exception
-                Output.Show(_TABLE_NAME & "->Link:" & ex.Message)
+                Output.Show(obj.GetType().Name & "(DAO) -> Link ")
+                Output.Show(ex.Message)
+                Output.Show(ex.StackTrace)
             End Try
 
             Return result
         End Function
 
         Public Function UnLink(ByVal fromId As Integer, ByVal toId As Integer) As Boolean Implements ILinkDAO.UnLink
+            Dim obj As New LnkApplicationSolutionToFunctionalCI()
             Dim result As Boolean = False
 
             Try
@@ -44,7 +41,7 @@ Namespace dal.dao
                 filter = filter & " AND " & LnkApplicationSolutionToFunctionalCI.C_FUNCTIONALCI_IDENTIFY & " = " & toId
 
                 Dim drs As List(Of DataRow)
-                drs = DataTables(_TABLE_NAME).Select(filter)
+                drs = DataTables(obj.GetType().Name).Select(filter)
 
                 For Each dr As DataRow In drs
                     dr(C__ISDELETED) = True
@@ -53,11 +50,28 @@ Namespace dal.dao
 
                 result = True
             Catch ex As Exception
-                Output.Show(_TABLE_NAME & "->UnLink:" & ex.Message)
+                Output.Show(obj.GetType().Name & "(DAO) -> UnLink ")
+                Output.Show(ex.Message)
+                Output.Show(ex.StackTrace)
             End Try
 
             Return result
         End Function
+
+        Private Sub TestFindList()
+            Dim dao As New ommp.dal.dao.LnkApplicationSolutionToFunctionalCIDAO
+            Dim lists As IList(Of ommp.dal.dto.LnkApplicationSolutionToFunctionalCI) = dao.FindList(Of ommp.dal.dto.LnkApplicationSolutionToFunctionalCI)("[applicationsolution_identify] = 21", "functionalci_identify")
+            Output.Show("Count : " & lists.Count)
+            Output.Show(" --------------------- ")
+            For Each dto As ommp.dal.dto.LnkApplicationSolutionToFunctionalCI In lists
+                Output.Show("Identify = " & dto._Identify)
+                Output.Show("functionalci_identify = " & dto.functionalci_identify)
+                Output.Show("applicationsolution_identify = " & dto.applicationsolution_identify)
+                Output.Show("IsDeleted = " & dto._IsDeleted)
+                Output.Show(" --------------------- ")
+            Next
+        End Sub
+
     End Class
 
 End Namespace
