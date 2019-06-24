5014effdbe17814318a9d86aa683f62691abbaa2
andylin <andy_linky@qq.com>
2019-06-24 14:32:23 +0800

    add GetComboListString(t)

--- dal/EntityDAO/FinalClasses/CodeDAO.vb
+++ dal/EntityDAO/FinalClasses/CodeDAO.vb
@@ -5,6 +5,12 @@ Namespace dal.dao
     Public Class CodeDAO
         Inherits GenericSimpleEntityDAO(Of Code)
 
+        Public Function GetComboListString(t As String) As String
+            Dim tableName As String = GetType(Code).Name
+
+            Return DataTables(tableName).GetComboListString("label", String.Format("[t]='{0}'", t), "v")
+        End Function
+
     End Class
 
 End Namespace
